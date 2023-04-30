47897f992848b8b413147528b83c4234f03b7eb2
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2023-04-30 09:36:26 +0530

    pointer

---
+++ Array_pointer/pointer.c
@@ -0,0 +1,16 @@
+
+
+#include<stdio.h>
+int main()
+{
+    int arr[5]={5,4,6,8,10};
+    int *p;
+    
+    for(int i=0;i<=4;i++){
+        *p=&arr[i];
+        printf("%d ",*p);
+    }
+    printf("\n%d",*(arr+2));
+
+    return 0;
+}
